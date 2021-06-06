da2b0314e8a0c242269a4cb7b23b94ae9b4aee2d
Shane O'Neill <soneill@asymmetrik.com>
2021-06-06 19:50:23 -0400

    remove default args

--- dags/one-two-three-dag.py
+++ dags/one-two-three-dag.py
@@ -6,34 +6,10 @@ from airflow.utils.dates import days_ago
 
 from constants.relationships import SUCCESS, FAILURE
 
-# These args will get passed on to each operator
-# You can override them on a per-task basis during operator initialization
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "email": ["airflow@example.com"],
-    "email_on_failure": False,
-    "email_on_retry": False,
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
-    # 'wait_for_downstream': False,
-    # 'dag': dag,
-    # 'sla': timedelta(hours=2),
-    # 'execution_timeout': timedelta(seconds=300),
-    # 'on_failure_callback': some_function,
-    # 'on_success_callback': some_other_function,
-    # 'on_retry_callback': another_function,
-    # 'sla_miss_callback': yet_another_function,
-    # 'trigger_rule': 'all_success'
-}
-
 
 @dag(
     "one-to-three",
-    default_args=default_args,
-    schedule_interval=timedelta(seconds=60),
+    # schedule_interval=timedelta(seconds=60),
     start_date=days_ago(2),
     tags=["example"],
     catchup=False,
